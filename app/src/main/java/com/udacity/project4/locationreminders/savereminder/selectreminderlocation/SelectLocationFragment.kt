1dd17e64d73c54982a481c0b44d68499de3b4445
ahmedAlameir <99804450+ahmedAlameir@users.noreply.github.com>
2022-10-31 21:25:28 +0200

    Initial commit

---
+++ app/src/main/java/com/udacity/project4/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
@@ -0,0 +1,188 @@
+package com.udacity.project4.locationreminders.savereminder.selectreminderlocation
+
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
+import android.content.IntentSender
+import android.content.pm.PackageManager
+import android.content.res.Resources
+import android.os.Bundle
+import android.util.Log
+import android.view.*
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.*
+import com.google.android.material.snackbar.Snackbar
+import com.udacity.project4.R
+import com.udacity.project4.base.BaseFragment
+import com.udacity.project4.base.NavigationCommand
+import com.udacity.project4.databinding.FragmentSelectLocationBinding
+import com.udacity.project4.locationreminders.savereminder.SaveReminderViewModel
+import com.udacity.project4.utils.setDisplayHomeAsUpEnabled
+import org.koin.android.ext.android.inject
+private const val TAG = "SaveReminderFragment"
+class SelectLocationFragment : BaseFragment(),OnMapReadyCallback {
+
+    //Use Koin to get the view model of the SaveReminder
+    override val _viewModel: SaveReminderViewModel by inject()
+    private lateinit var binding: FragmentSelectLocationBinding
+    private lateinit var map: GoogleMap
+    private lateinit var pointOfInterest: PointOfInterest
+    private val REQUEST_LOCATION_PERMISSION = 1
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_select_location, container, false)
+
+        binding.viewModel = _viewModel
+        binding.lifecycleOwner = this
+        val mapFragment = childFragmentManager.findFragmentById(R.id.select_map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+        setHasOptionsMenu(true)
+        setDisplayHomeAsUpEnabled(true)
+
+
+//        TODO: zoom to the user location after taking his permission
+
+
+
+
+
+        binding.saveButton.setOnClickListener {
+            onLocationSelected()
+        }
+        return binding.root
+    }
+
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+
+        //Default location
+        val sydney = LatLng(-34.0, 151.0)
+        val zoomLevel = 15f
+        map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, zoomLevel))
+        map.uiSettings.isZoomControlsEnabled = true
+        setPoiClick(map)
+        setMapStyle(map)
+        enableMyLocation()
+    }
+    private fun setPoiClick(map: GoogleMap) {
+        map.setOnPoiClickListener { poi ->
+            pointOfInterest = poi
+            val poiMarker = map.addMarker(
+                MarkerOptions()
+                    .position(poi.latLng)
+                    .title(poi.name)
+            )
+            poiMarker.showInfoWindow()
+        }
+    }
+
+    private fun setMapStyle(map: GoogleMap)  {
+        try {
+            val success = map.setMapStyle(
+                MapStyleOptions.loadRawResourceStyle(
+                    requireContext(),
+                    R.raw.map_style
+                )
+            )
+            if (!success) {
+                Log.e(TAG, "Style parsing failed.")
+            }
+        } catch (e: Resources.NotFoundException) {
+            Log.e(TAG, "Can't find style. Error: ", e)
+        }
+    }
+
+    private fun onLocationSelected() {
+        if (this::pointOfInterest.isInitialized) {
+            _viewModel.latitude.value = pointOfInterest.latLng.latitude
+            _viewModel.longitude.value = pointOfInterest.latLng.longitude
+            _viewModel.reminderSelectedLocationStr.value = pointOfInterest.name
+            _viewModel.selectedPOI.value=pointOfInterest
+            findNavController().popBackStack()
+        } else {
+            val toast = Toast.makeText(context, resources.getString(R.string.select_location), Toast.LENGTH_SHORT)
+            toast.show()
+        }
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.map_options, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+
+        R.id.normal_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_NORMAL
+            true
+        }
+        R.id.hybrid_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_HYBRID
+            true
+        }
+        R.id.satellite_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_SATELLITE
+            true
+        }
+        R.id.terrain_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_TERRAIN
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
+    }
+
+    private fun isPermissionGranted() : Boolean {
+        return context?.let {
+            ContextCompat.checkSelfPermission(
+                it,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        } == PackageManager.PERMISSION_GRANTED
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun enableMyLocation() {
+        if (isPermissionGranted()) {
+            map.setMyLocationEnabled(true)
+        }
+        else {
+            ActivityCompat.requestPermissions(
+                context as Activity,
+                arrayOf<String>(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_LOCATION_PERMISSION
+            )
+        }
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray) {
+        // Check if location permissions are granted and if so enable the
+        // location data layer.
+        if (requestCode == REQUEST_LOCATION_PERMISSION) {
+            if (grantResults.size > 0 && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                enableMyLocation()
+            }
+        }
+    }
+
+
+}
---
+++ app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
@@ -0,0 +1,82 @@
+package com.udacity.project4.locationreminders.reminderslist
+
+import android.os.Build
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.udacity.project4.locationreminders.MainCoroutineRule
+import com.udacity.project4.locationreminders.data.FakeDataSource
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
+import com.udacity.project4.locationreminders.getOrAwaitValue
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.hamcrest.MatcherAssert
+import org.hamcrest.Matchers
+import org.hamcrest.Matchers.`is`
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.koin.core.context.stopKoin
+import org.robolectric.annotation.Config
+
+@RunWith(AndroidJUnit4::class)
+@ExperimentalCoroutinesApi
+@Config(maxSdk = Build.VERSION_CODES.P)
+
+class RemindersListViewModelTest {
+
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    val mainCoroutineRule = MainCoroutineRule()
+
+    private lateinit var fakeRepo: FakeDataSource
+    private lateinit var remindersListViewModel: RemindersListViewModel
+
+    @Before
+    fun createRepository() {
+        stopKoin()
+
+        fakeRepo = FakeDataSource()
+        remindersListViewModel = RemindersListViewModel(
+            getApplicationContext(),
+            fakeRepo
+        )
+    }
+
+    @Test
+    fun check_loading() = runBlockingTest {
+        fakeRepo.deleteAllReminders()
+        val reminder = ReminderDTO("Title", "Description", "Location", 19.0, 20.2)
+        fakeRepo.saveReminder(reminder)
+
+        mainCoroutineRule.pauseDispatcher()
+
+        remindersListViewModel.loadReminders()
+        MatcherAssert.assertThat(
+            remindersListViewModel.showLoading.getOrAwaitValue(),`is`(true)
+        )
+
+        mainCoroutineRule.resumeDispatcher()
+        MatcherAssert.assertThat(
+            remindersListViewModel.showLoading.getOrAwaitValue(), `is`(false)
+        )
+    }
+    @Test
+    fun shouldReturnError() = runBlockingTest {
+        fakeRepo.setReturnsError(true)
+        remindersListViewModel.loadReminders()
+
+        MatcherAssert.assertThat(
+            remindersListViewModel.showSnackBar.getOrAwaitValue(),
+            Matchers.`is`(Matchers.notNullValue())
+        )
+    }
+
+
+    //TODO: provide testing to the RemindersListViewModel and its live data objects
+
+}
